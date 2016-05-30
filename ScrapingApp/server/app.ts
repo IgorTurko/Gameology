959320ab81898bd4e50c7bfdbe3b3c1b950bad8a
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-05-30 16:12:13 +0300

    Add sample server for static files

--- ScrapingApp/server/app.ts
+++ ScrapingApp/server/app.ts
@@ -1,1 +1,13 @@
 ﻿/// <reference path="./typings/index.d.ts"/>
+const http = require("http");
+const express = require("express");
+
+let server = express();
+
+server.use("/", express.static("./public", {
+    index: "index.html"
+}));
+
+server.listen(3000, () => {
+    console.log("Web server started at http://localhost:3000/");
+});
