9fb6e96cf8a754173fca9eb409cff04be28f096f
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-05-31 15:42:11 +0300

    Install mongodb package Install mongodb typings

--- ScrapingApp/server/typings/index.d.ts
+++ ScrapingApp/server/typings/index.d.ts
@@ -2,4 +2,6 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />
 /// <reference path="modules/mime/index.d.ts" />
+/// <reference path="modules/mongodb/index.d.ts" />
