6211c73a99170e5825ae64cc84dba690ee25988e
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-06-14 10:44:20 +0300

    Delete components folder

--- ScrapingApp/client/components/pages/product-page.tsx
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="./../../typings/index.d.ts" />
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-export default class ProductPage extends React.Component<{}, {}> {
-    render() {
-        return (<div>ProductPage</div>);
-    }
-}
